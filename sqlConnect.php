0a23694072829535607536fbabfa026e6fc322d5
H2oFall <42394085+H2oFall@users.noreply.github.com>
2020-02-14 08:33:04 -1000

    Add files via upload

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,30 +1,14 @@
-<?php
+ <?php
 $servername = "localhost";
 $username = "user";
 $password = "password";
-$dbname = "school";
-
-$name = "Jorge";
-$age = 12;
-$gradeLevel = 11;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-
-$sql = "INSERT INTO students (name, age, gradeLevel)
-VALUES ('$name', '$age', '$gradeLevel')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?> 
